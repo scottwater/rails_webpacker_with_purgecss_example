6252b01ceaf5f3cf23aa203436280fa0709d9e0f
Scott Watermasysk <scottwater@gmail.com>
2019-01-26 17:33:59 -0500

    setup purge

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,29 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const path = require('path')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+
+
+// ensure classes with special chars like -mt-1 and md:w-1/3 are included
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
+environment.plugins.append('PurgecssPlugin', new PurgecssPlugin({
+  paths: glob.sync([
+    path.join(__dirname, '../../app/javascript/**/*.js'),
+    path.join(__dirname, '../../app/views/**/*.erb')
+  ]),
+  extractors: [ // if using Tailwind
+    {
+      extractor: TailwindExtractor,
+      extensions: ['html', 'js', 'erb']
+    }
+  ]
+}));
+
 module.exports = environment.toWebpackConfig()
